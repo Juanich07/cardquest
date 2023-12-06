eac2a30a0746b8d6d5490c49901d34b692235178
Juanich07 <202180228@psu.palawan.edu.ph>
2023-12-07 05:31:33 +0800

    new

--- appdev01/projectsite/cardquest/views.py
+++ appdev01/projectsite/cardquest/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.views.generic.list import ListView
+from cardquest.models import PokemonCard
+
+class HomePageView(ListView):
+    model = PokemonCard
+    context_object_name = 'home'
+    template_name = "home.html"
+
+    def get_context_data(self, **kwargs):
+        context = super(HomePageView, self).get_context_data(**kwargs)
+        return context
