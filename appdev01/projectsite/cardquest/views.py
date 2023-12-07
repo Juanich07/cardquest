2329fa8a3f3eb5885ae8169a63e8550c9c2ab719
Juanich07 <202180228@psu.palawan.edu.ph>
2023-12-07 21:06:56 +0800

    new

--- appdev01/projectsite/cardquest/views.py
+++ appdev01/projectsite/cardquest/views.py
@@ -1,14 +1,41 @@
+# from django.shortcuts import render
+
+# # Create your views here.
+# from django.views.generic.list import ListView
+# from cardquest.models import PokemonCard, Trainer
+# class HomePageView(ListView):
+#     model = PokemonCard
+#     context_object_name = 'home'
+#     template_name = "home.html"
+
+
+# class TrainerList(ListView):
+#     model = Trainer
+#     context_object_name = 'trainer'
+#     template_name = "trainer.html"
+#     paginate_by = 15
+
+#     def get_context_data(self, **kwargs):
+#         context = super().get_context_data(**kwargs)
+#         return context
+#         # context = super(HomePageView, self).get_context_data(**kwargs)
+#         # return context
 from django.shortcuts import render
 
-# Create your views here.
 from django.views.generic.list import ListView
-from cardquest.models import PokemonCard
+from cardquest.models import PokemonCard, Trainer
 
 class HomePageView(ListView):
     model = PokemonCard
     context_object_name = 'home'
-    template_name = "home.html"
+    template_name = "base.html"
 
     def get_context_data(self, **kwargs):
-        context = super(HomePageView, self).get_context_data(**kwargs)
+        context = super().get_context_data(**kwargs)
         return context
+
+class TrainerList(ListView):
+    model = Trainer
+    context_object_name = 'trainer'
+    template_name = 'trainers.html'
+    paginate_by = 15
--- appdev01/projectsite/projectsite/urls.py
+++ appdev01/projectsite/projectsite/urls.py
@@ -14,12 +14,33 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+# from django.contrib import admin
+# from django.urls import path
+# from cardquest import views
+# from cardquest.views import HomePageView
+# from cardquest.views import HomePageView, TrainerList
+
+# urlpatterns = [
+#     path('admin/', admin.site.urls),
+#     path('', views.HomePageView.as_view(), name='home'),
+#     path('trainer_list', TrainerList.as_view(), name='trainer-list'),
+# ]
+# from django.contrib import admin
+# from django.urls import path
+# from cardquest.views import HomePageView, TrainerList
+
+# urlpatterns = [
+#     path('admin/', admin.site.urls),
+#     path('', HomePageView.as_view(), name='home'),
+#     path('trainer_list/', TrainerList.as_view(), name='trainer-list'),  # Added a trailing slash
+# ]
 from django.contrib import admin
 from django.urls import path
-from cardquest.views import HomePageView
+from cardquest.views import HomePageView, TrainerList
 from cardquest import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.HomePageView.as_view(), name='home'),
+    path('trainer_list', TrainerList.as_view(), name='trainer-list'),
 ]
